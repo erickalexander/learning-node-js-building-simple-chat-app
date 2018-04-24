00f4a7c159070567d1f5ae8a531688ee0f01f96d
erickalexander <erickcamacho1@gmail.com>
2018-04-24 16:07:49 -0400

    created route for get and post in backend, made HTTP Ajax requests in the front-end using JQuery and installed body parser send JSON

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var express = require('express') //tells application it will be using express
+var bodyParser = require('body-parser')
 var app = express()
 
+
 app.use(express.static(__dirname)) //serves the static html file
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false})) //catch the values from the front end which come in urlencoded
+
+var messages = [
+  {name: 'jerry', message:'Hi'},
+  {name: 'steve', message:'Hi'}
+
+]
+app.get('/messages',(req,res) => {
+
+  res.send(messages)
+})
+
+app.post('/messages',(req,res) => {
+
+  messages.push(req.body)
+  res.sendStatus(200)
+})
 
 var server = app.listen(3000, () =>{
 
